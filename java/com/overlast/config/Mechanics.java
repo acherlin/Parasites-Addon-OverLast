59b918051e21aee71094858d9f6caab15b58ff6f
acherlin <66243309+acherlin@users.noreply.github.com>
2022-02-22 13:51:27 +0800

    Update Mechanics.java

--- java/com/overlast/config/Mechanics.java
+++ java/com/overlast/config/Mechanics.java
@@ -10,4 +10,8 @@ public class Mechanics {
     @Config.RangeDouble(min = 0, max = 10.0)
     public double naturalEvolutionScale = 1.0;
 
+    @Config.LangKey("config.overlast:mechanics.showRequestDirtyClock")
+    @Config.RequiresWorldRestart
+    public boolean showRequestDirtyClock = false;
+    
 }
